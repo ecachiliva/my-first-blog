65989629203d8e4bbac9ec6194d121aab02c2c06
ecachiliva <emiguelcachili@gmail.com>
2020-09-26 22:16:40 +0100

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Publicar
 
 def publicar_list(request):
-	return render(request, 'blog/publicar_list.html')
+	posts = Publicar.objects.filter(data_publicar__lte=timezone.now()).order_by('data_publicar')
+	return render(request, 'blog/publicar_list.html', {'posts': posts})
