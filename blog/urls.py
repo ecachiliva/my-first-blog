674c6c4247b8951317f7cf4767c7ccc2e2a80d8f
ecachiliva <emiguelcachili@gmail.com>
2020-09-27 03:28:15 +0100

    Adicionamos views publicar_list/publicar_detalle blog publicar

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import publicar_list
+from .views import publicar_list, inicio, publicar_detalle
 urlpatterns = [
 
-	#path('', inicio, name="inicio"),
+	# path('', inicio, name="inicio"),
 	path('', publicar_list, name="publicar_list"),
+	#path('<int:pk>/', publicar_detalle, name="publicar_detalle"),
+	path('<int:pk>/editar/', publicar_detalle, name='publicar_detalle'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Publicar
+
+
+
+def inicio(request):
+	posts = Publicar.objects.filter(data_publicar__lte=timezone.now()).order_by('data_publicar')
+	return render(request, 'blog/index.html', {'posts': posts})
 
 def publicar_list(request):
 	posts = Publicar.objects.filter(data_publicar__lte=timezone.now()).order_by('data_publicar')
 	return render(request, 'blog/publicar_list.html', {'posts': posts})
+
+def publicar_detalle(request, pk):
+	post = get_object_or_404(Publicar, pk=pk)
+	return render(request, 'blog/publicar_detalle.html', {'post': post})
+
